c6e35eabf680bdab89e01508b05974f9ed367904
yojuancarlos <56267375+yojuancarlos@users.noreply.github.com>
2022-10-03 11:19:59 -0500

    parametros

--- hola.py
+++ hola.py
@@ -1,1 +1,17 @@
-print("hola mudsdfd")
+def parametros ():
+    cantidad_terminal= int
+    lista_terminales=[]
+    lista_noterminales=[]
+
+    cantidad_terminal = input("escriba la cantidad de termniales que desea ingresar:")
+
+    for i in range(int(cantidad_terminal)):
+
+        lista_terminales.append(input("escriba el termninal : "))
+
+        lista_noterminales.append(input("escriba el no terminal : "))
+
+    print(lista_terminales)
+    print(lista_noterminales)
+
+parametros()
